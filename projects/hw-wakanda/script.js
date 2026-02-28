de766139d13261081cf99248afed16ea5b1b9815
Yuriy Buchko <158298702+gLuck1337@users.noreply.github.com>
2026-02-28 16:32:52 +0200

    Delete projects/hw-wakanda directory

--- projects/hw-wakanda/script.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementById("burgerBtn");
-
-burger.addEventListener("click", () => {
-  const menu = document.getElementById("nav__menu");
-  menu.classList.toggle("nav__menu--active");
-});
