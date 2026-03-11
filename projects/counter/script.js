eb4f3fef6cc2c63600485c633fe7b6076c89eb75
Yuriy <buchko11@icloud.com>
2026-03-12 00:08:36 +0200

    counter-add-and-remove-class

--- projects/counter/script.js
+++ projects/counter/script.js
@@ -1,19 +1,33 @@
-const counters = document.querySelectorAll('[data-counter]');
+const counters = document.querySelectorAll("[data-counter]");
 
 if (counters) {
-  counters.forEach (counter => {
-    counter.addEventListener('click', e => {
+  counters.forEach((counter) => {
+    counter.addEventListener("click", (e) => {
       const target = e.target;
 
-      if (target.closest('.counter__button')) {
+      if (target.closest(".counter__button")) {
         let value = parseInt(
-          target.closest('.counter').querySelector('input').value,
+          target.closest(".counter").querySelector("input").value,
         );
 
-        if (target.classList.contains('counter__button-plus')) {
+        if (target.classList.contains("counter__button-plus")) {
           value++;
         } else {
           --value;
+        }
+
+        if (value <= 0) {
+          value = 0;
+
+          target
+            .closest(".counter")
+            .querySelector(".counter__button-minus")
+            .classList.add("disabled");
+        } else{
+          target
+            .closest(".counter")
+            .querySelector(".counter__button-minus")
+            .classList.remove("disabled");
         }
 
         target.closest(".counter").querySelector("input").value = value;
